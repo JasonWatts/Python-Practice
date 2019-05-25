723cae6505a56e2227ed5e2865f31843cb726a7b
JasonWatts <jawatts@westmont.edu>
2019-05-25 13:16:23 -0700

    q2 done

---
+++ q2.py
@@ -0,0 +1,17 @@
+"""
+Question:
+Write a program which can compute the factorial of a given numbers.The results should be printed in a comma-separated sequence on a single line.Suppose the following input is supplied to the program: 8 Then, the output should be:40320
+
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
+"""
+
+
+def factorial(i):
+    if i == 0:
+        return 1
+    return i * factorial(i - 1)
+
+i = int(input("enter a number: "))
+
+print(factorial(i))
